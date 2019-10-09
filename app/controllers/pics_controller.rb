bf55239e242a61a56f9fecdf612314e4735a7caa
Dalep <dalepaulsen.code.social@gmail.com>
2019-10-09 18:05:50 +1000

    added crud methods in pics_controller with corresponding view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,4 +3,21 @@ class PicsController < ApplicationController
   # 1
   def index
   end
+
+  #2-new and create will correspond to new.html view file
+  def new
+    @pic = Pic.new
+  end
+
+  #3-now i need to define the params in a private method. Once params are created in a priv meth i can re-use them and keep code #DRY
+  def create
+    @pic = Pic.new(pic_params)
+  end
+
+  private
+
+  #4
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
 end
